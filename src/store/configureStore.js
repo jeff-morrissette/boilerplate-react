ed46fbf3a1416ef355ecbbfe161a8d9b86f7d390
Jeff <bin_aural@hotmail.com>
2017-12-19 12:56:04 -0600

    initial commit

---
+++ src/store/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import authReducer from '../reducers/auth';
+
+// The Line below is to allow the redux tab in web developer tool in firefox to work
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// Store creation
+export default () => {
+	const store = createStore(
+		combineReducers({
+			auth: authReducer
+		}),
+		composeEnhancers(applyMiddleware(thunk))
+	);
+
+	return store;
+};
